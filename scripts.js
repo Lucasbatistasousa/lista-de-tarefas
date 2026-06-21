9263d449c7648373fd700e9a03973d10b42a7b6b
Lucas Batista <lucasbatistasousa2002@gmail.com>
2026-06-21 16:38:02 -0300

    Adição da verificação se o input está vazio e a troca do emoji pelo risco na tarefa concluida

--- scripts.js
+++ scripts.js
@@ -5,24 +5,33 @@ function adicionarTarefa(){
     let tarefa = document.getElementById("tarefa") // Variável que recebe o input no HTML
     let texto = tarefa.value; // Variável que vai ser adicionada no valor do objeto tarefa.
 
-    // Faz o push para adicionar o valor do input na lista
-    tarefas.push({
-        tarefa: texto,
-        concluida: false,
-    });
+    // Utilização do método para tira os espaços nas extremidades das strings
+    let textoFormatado = texto.trim();
 
-    // Chamada de outra função
-    atualizarLista();
+    // Condição para o input não ser vazio
+    if (textoFormatado === ""){
+        window.alert("Você precisa adicionar uma tarefa válida!")
+    } else {
+        // Faz o push para adicionar o valor do input na lista
+        tarefas.push({
+            tarefa: textoFormatado,
+            concluida: false,
+        });
+        // Chamada de outra função
+         atualizarLista();
 
-    // Atualiza o input para vazio depois de adicionar a tarefa na lista.
-    tarefa.value = "";
+        // Atualiza o input para vazio depois de adicionar a tarefa na lista.
+        tarefa.value = "";
+    }
+
+    
 }
 
 // Função que atualiza a lista
 function atualizarLista(){
     let lista = document.getElementById("lista") // Variável que pega a lista no HTML
 
-    // Garantindo que os itens da lista não fiquem duplicados
+    // Essa linha garante que os itens da lista não fiquem duplicados
     lista.innerHTML = "";
 
     for(let i = 0; i < tarefas.length; i++){
@@ -31,8 +40,8 @@ function atualizarLista(){
         let texto = tarefas[i].tarefa;
 
         // Verifica se o valor da chave concluida é true
-        if (tarefas[i].concluida === true){
-            texto = `✅ ${texto}`; // Se a tarefa tiver sido concluida adiciona o emoji
+        if (tarefas[i].concluida){
+            texto = `<s>${texto}`; // Se a tarefa tiver sido concluida ela é riscada.
         }
 
         // Mostra a lista na tela com botões para remover e concluir tarefa
